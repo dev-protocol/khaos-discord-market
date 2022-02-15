2c46065e99ff7da6cf5d094b99a870a77e92d33e
CryptoDogg <d.enkikairo@gmail.com>
2022-02-15 10:26:09 +0900

    add testnet addresses

--- src/addresses.ts
+++ src/addresses.ts
@@ -2,11 +2,11 @@ import { FunctionAddresses } from '@devprotocol/khaos-core'
 
 export const addresses: FunctionAddresses = async ({ network: net }) =>
 	net === 'arbitrum-rinkeby'
-		? undefined
+		? '0x78D3dA4dAFB2c51C0Ee7e598C871ce4d73cF728e'
 		: net === 'polygon-mumbai'
-		? undefined
-		: net === 'polygon-mainnet'
-		? undefined
-		: net === 'arbitrum-one'
-		? undefined
-		: undefined
+			? '0x18664CD5dFF515Da2304c80E4c9216e873A4C96E'
+			: net === 'polygon-mainnet'
+				? undefined
+				: net === 'arbitrum-one'
+					? undefined
+					: undefined
